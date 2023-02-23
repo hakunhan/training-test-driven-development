691835573d6384e913f93a92876faedfe10c34c9
hoanghieu <hakunhan@gmail.com>
2023-02-23 14:39:26 +0700

    Update to requirement 4 of StringCal

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -1,33 +1,98 @@
 package org.example;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StringCalculator {
-    /**
-     * Requirement 1:
-     * Take upto two number that seperated by a comma then return sum of that number. If that string is empty, then return 0.
-     * Ex: "1,2" => 3
-     *
-     * @param numbers string contains upto two numbers
-     * @return sum of input
-     */
-    public int add(String numbers) throws IllegalArgumentException{
-        int result = 0;
+	/**
+	 * Requirement 1:
+	 * Take upto two numbers that seperated by a comma then return sum of that number. If that string is empty, then return 0.
+	 * Ex: "1,2" => 3
+	 * Requirement 2:
+	 * Also take n number of numbers that seperated by commas then return sum of that number.
+	 * Requirement 3:
+	 * Also take newlines as separators
+	 * Requirement 4:
+	 * Not allow a separator at the end of string
+	 *
+	 * @param input string contains upto two numbers
+	 * @return sum of input
+	 */
+	public int add(String input) throws IllegalArgumentException {
+		int result = 0;
+		List<String> listOfNumbers = new ArrayList<>();
 
-        if (numbers == null || numbers.isEmpty() || numbers.isBlank())
-            return result;
+		if (input == null || input.isEmpty() || input.isBlank()) return result;
 
-        if (numbers.contains(",")) {
-            if (!numbers.matches((".*\\d+.*"))) {
-                throw new IllegalArgumentException("Input only contains characters");
-            }
-        }
+		if (input.length() == 1) {
+			return Integer.parseInt(input);
+		}
 
-        String[] listOfNumber = numbers.split(",");
-        for (String number: listOfNumber) {
-            result += Integer.parseInt(number);
-        }
+		validateInput(input);
 
-        return result;
-    }
+		if (input.contains(",")) {
+			listOfNumbers.addAll(separatedInput(input, ","));
+			input = clearSeparatedNumber(listOfNumbers, input, ",");
+		}
+
+		if (input.contains("\n")) {
+			listOfNumbers.addAll(separatedInput(input, "\n"));
+			input = clearSeparatedNumber(listOfNumbers, input, "\n");
+		}
+
+		for (String number : listOfNumbers) {
+			if (number.isBlank()) {
+				continue;
+			}
+
+			result += Integer.parseInt(number);
+		}
+
+		return result;
+	}
+
+	private void validateInput(String input) {
+		if (!containsDigit(input)) {
+			throw new IllegalArgumentException("Input only contains characters");
+		}
+
+		if (!Character.isDigit(input.charAt(input.length() - 1))) {
+			throw new IllegalArgumentException("Separator cannot be at the end of input");
+		}
+	}
+
+	private boolean containsDigit(final String aString) {
+		return aString != null && !aString.isEmpty() && aString.chars().anyMatch(Character::isDigit);
+	}
+
+	/**
+	 * Separate input of numbers into list of string number with separator
+	 *
+	 * @param input     list of numbers with separator
+	 * @param separator a mark that indicate input have multiple number
+	 * @return list of numbers without provided separator
+	 */
+	private List<String> separatedInput(String input, String separator) {
+		List<String> result = new ArrayList<>();
+
+		String[] listOfNumbers = input.split(separator);
+
+		for (String number : listOfNumbers) {
+			if (number.matches("\\d+")) {
+				result.add(number);
+			}
+		}
+
+		return result;
+	}
+
+	private String clearSeparatedNumber(List<String> numbers, String input, String separator) {
+		String result = "";
+		for (String number : numbers) {
+			result = input.replace(number, "");
+			result = result.replace(separator, "");
+		}
+
+		return result;
+	}
 }
--- src/test/java/org/example/StringCalculatorTest.java
+++ src/test/java/org/example/StringCalculatorTest.java
@@ -2,47 +2,68 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class StringCalculatorTest {
-    StringCalculator calculator = new StringCalculator();
+	StringCalculator calculator = new StringCalculator();
 
-    @Test
-    void expectedReturn0ForEmptyString() {
-        String empty = "";
-        assertEquals(0, calculator.add(empty));
-    }
+	@Test
+	void expectedReturn0ForEmptyString() {
+		String empty = "";
+		assertEquals(0, calculator.add(empty));
+	}
 
-    @Test
-    void expectedReturn0ForNullString() {
-        assertEquals(0, calculator.add(null));
-    }
+	@Test
+	void expectedReturn0ForNullString() {
+		assertEquals(0, calculator.add(null));
+	}
 
-    @Test
-    void expectedReturn0ForBlankString(){
-        assertEquals(0, calculator.add("  "));
-    }
+	@Test
+	void expectedReturn0ForBlankString() {
+		assertEquals(0, calculator.add("  "));
+	}
 
-    @Test
-    void expectedReturn0ForOnlyContainsCommaString() {
-        assertThrows(IllegalArgumentException.class, () -> calculator.add(","));
-    }
+	@Test
+	void expectedReturn0ForOnlyContainsCommaString() {
+		assertThrows(IllegalArgumentException.class, () -> calculator.add(","));
+	}
 
-    @Test
-    void expectedThrowExceptionIfStringContainsOneNumber() {
-        String oneNumberString = "5";
-        assertEquals(5, calculator.add(oneNumberString));
-    }
+	@Test
+	void expectedThrowExceptionIfStringContainsOneNumber() {
+		String oneNumberString = "5";
+		assertEquals(5, calculator.add(oneNumberString));
+	}
 
-    @Test
-    void expectedReturnThatNumberIfStringContainsANumberAndASeparate(){
-        String oneNumberAndASeparateString = "5,";
-        assertEquals(5, calculator.add(oneNumberAndASeparateString));
-    }
+	@Test
+	void expectedReturnSumOfTwoNumberIfStringContainsTwoNumberAndSeparate() {
+		String twoNumberAndASeparateString = "5,10";
+		assertEquals(15, calculator.add(twoNumberAndASeparateString));
+	}
 
-    @Test
-    void expectedReturnSumOfTwoNumberIfStringContainsTwoNumberAndSeparate() {
-        String twoNumberAndASeparateString = "5,10";
-        assertEquals(15, calculator.add(twoNumberAndASeparateString));
-    }
+	@Test
+	void expectedReturnSumOfMoreThanTwoNumberIfStringContains() {
+		String multipleNumberAndSeparatedString = "1,2,3,4,5,6";
+		assertEquals(21, calculator.add(multipleNumberAndSeparatedString));
+	}
+
+	@Test
+	void expectedReturnSumWithNewLineAsSeparator() {
+		String newLineSeparator = "1\n3";
+		assertEquals(4, calculator.add(newLineSeparator));
+	}
+
+	@Test
+	void expectedReturnSumWithNewLineAndCommaAsSeparator() {
+		String input = "1,2\n3";
+		assertEquals(6, calculator.add(input));
+		String input2 = "3\n2,1";
+		assertEquals(6, calculator.add(input2));
+	}
+
+	@Test
+	void expectedThrowErrorIfSeparatorAtTheEndOfInput() {
+		String input = "5,6,";
+		assertThrows(IllegalArgumentException.class, () -> calculator.add(input));
+	}
 }
